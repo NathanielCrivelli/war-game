122d0473600eda2c078e9004dfa3028066c1a152
Andrew Maldonado <andrew.maldonado@mrjaffesclass.com>
2022-11-28 12:51:06 -0800

    Nov 28

--- War.java
+++ War.java
@@ -40,24 +40,72 @@ public class War
      * from the War flowchart you created for this game
      */
     public void runEventLoop() {
-        //ArrayList dealtList = new ArrayList();
-        ArrayList<Card> dealtList = new ArrayList();
-        ArrayList<Card> dealtList2 = new ArrayList();
         while (player1.getDeckSize() > 0 && player2.getDeckSize() > 0) {
-            //System.out.println("hi");
-            dealtList.add(player1.dealCardFromDeck());
-            dealtList.add(player2.dealCardFromDeck());
-            // int i = 0;
-            if(dealtList.getRank() == dealtList2.getRank()) {
-                 System.out.println("hi");
-            
-            } 
-            for (int i = 0; i < dealtList.size(); i++){
-                 System.out.print(dealtList.get(i).getRank());
-                 System.out.print(", ");
-                    player1.addCardToDeck();
-            }   
-            System.out.println("");
+            Card card1 = player1.dealCardFromDeck();
+            Card card2 = player2.dealCardFromDeck();
+            if (card1.getRank() > card2.getRank()) {
+                player1.addCardToDeck(card1);
+                player1.addCardToDeck(card2);
+                System.out.println("P1 won round");
+                System.out.println("P1: " + player1.getDeckSize());
+                System.out.println("P2: " + player2.getDeckSize());
+            } else if (card1.getRank() < card2.getRank()) {
+                player2.addCardToDeck(card1);
+                player2.addCardToDeck(card2);
+                System.out.println("P2 won round");
+                System.out.println("P1: " + player1.getDeckSize());
+                System.out.println("P2: " + player2.getDeckSize());
+            } else {
+                ArrayList<Card> war1 = new ArrayList<Card>();
+                ArrayList<Card> war2 = new ArrayList<Card>();
+                war1.add(card1);
+                war2.add(card2);
+                while (card1.getRank() == card2.getRank()) {
+                    for (int i = 0; i < 3; i++) {
+                        if (player1.getDeckSize() > 0) {
+                            war1.add(player1.dealCardFromDeck());
+                        }
+                        if (player2.getDeckSize() > 0) {
+                            war2.add(player2.dealCardFromDeck());
+                        }
+                    }
+                    // if (player1.getDeckSize() <= 0) {
+                    //     card1 = player1.dealCardFromDeck();
+                    //     war1.add(card1);
+                    //     System.out.println("P1 Loser");
+                    // }   
+                    // if (player2.getDeckSize() <= 0) {
+                    //     card2 = player2.dealCardFromDeck();
+                    //     war2.add(card2);
+                    //     System.out.println("P2 Loser");
+                    // }
+                }
+                if (card1.getRank() > card2.getRank()) {
+                    for (int i = 0; i < war1.size(); i++) {
+                        player1.addCardToDeck(war1.get(i));
+                    }
+                    for (int i = 0; i < war2.size(); i++) {
+                        player1.addCardToDeck(war2.get(i));
+                    }
+                } else if (card1.getRank() < card2.getRank()) {
+                    for (int i = 0; i < war1.size(); i++) {
+                        player2.addCardToDeck(war1.get(i));
+                    }
+                    for (int i = 0; i < war2.size(); i++) {
+                        player2.addCardToDeck(war2.get(i));
+                    }
+                }
+                if (player1.getDeckSize() <= 0) {
+                    card1 = player1.dealCardFromDeck();
+                    war1.add(card1);
+                    System.out.println("P1 Loser");
+                }
+                if (player2.getDeckSize() <= 0) {
+                    card2 = player2.dealCardFromDeck();
+                    war2.add(card2);
+                    System.out.println("P2 Loser");
+                }
+            }
         }
     }
 
